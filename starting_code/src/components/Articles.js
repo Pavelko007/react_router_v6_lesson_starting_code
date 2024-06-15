279510d30d6663b4341bbcf89b62396ece325fd8
Pavlo Kvas <kvaspasha@gmail.com>
2024-06-15 14:36:05 +0300

    Query Parameters

--- starting_code/src/components/Articles.js
+++ starting_code/src/components/Articles.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectArticles, filterArticles } from "../features/articles/articlesSlice";
 import Search from "./Search";
@@ -8,11 +8,11 @@ import Search from "./Search";
 
 export default function Articles () {
   const articles = useSelector(selectArticles);
-
   // Grab URLSearchParams object from useSearchParams hook
-  
+  const [searchParams, setSearchParams] = useSearchParams();
+
   // Get the queryParams from object returned from useSearchParams and set to `title`
-  const title = '';
+  const title = searchParams.get('title');
 
   const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles)
 
